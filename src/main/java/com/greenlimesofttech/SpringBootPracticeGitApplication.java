cecadc97684afdf9915c8bcad7436411315c70d9
swapnilthakre468 <tswapnil468@gmail.com>
2023-10-08 22:51:06 +0530

    initial commit

---
+++ src/main/java/com/greenlimesofttech/SpringBootPracticeGitApplication.java
@@ -0,0 +1,13 @@
+package com.greenlimesofttech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenlimesofttech/SpringBootPracticeGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenlimesofttech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
